d600e04684682150852878d6fd90dfd4f29be332
y.yanin <yanin.yarosla7@gmail.com>
2022-12-22 21:03:00 +0300

    first commit

---
+++ src/pro/sky/java/course1/courseWork1/Employee.java
@@ -0,0 +1,4 @@
+package pro.sky.java.course1.courseWork1;
+
+public class Employee {
+}
---
+++ src/pro/sky/java/course1/courseWork1/Main.java
@@ -0,0 +1,9 @@
+package pro.sky.java.course1.courseWork1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
